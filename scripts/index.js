6e8a03347e296596e1ad0dff9ff8544b91a63b44
Lucas Rodrigues <lucas.movimento@gmail.com>
2024-09-10 17:47:00 -0300

    feat: Add style and logic to get values also add component to show response

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,56 @@
 function updateRangeInput(val, el) {
-    document.getElementById(el).value = val; 
+  document.getElementById(el).value = val;
+}
+
+const dataForm = document.getElementById("dataForm");
+
+dataForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  // const baseUrl = "http://127.0.0.1:5000/ml";
+  // let url = "";
+
+  const gender = Array.from(
+    document.getElementsByName("genderRadioInput")
+  ).find((radio) => radio.checked);
+
+  const data = {
+    age: document.getElementById("rangeAgeInput").value,
+    gender: gender.value,
+    educationLevel: document.getElementById("educationSelect").value,
+    experienceYears: document.getElementById("rangeXpInput").value,
+    previoousCompanies: document.getElementById("rangePrevCiaInput").value,
+    companyDistance: document.getElementById("rangeDistCiaInput").value,
+    interviewScore: document.getElementById("rangeInterviewScoreInput").value,
+    skillScore: document.getElementById("rangeSkillScoreInput").value,
+    personalityScore: document.getElementById("rangePersonalityScoreInput")
+      .value,
+    recruitmentStrategy: document.getElementById("strategySelect").value,
+  };
+
+  for (el in data) {
+    if (data[el] == "") {
+      alert(`${el} is required!`);
+      return;
+    }
   }
+
+  // const response = await handleRequest(url, "POST", data).then(
+  //   (result) => result
+  // );
+  // const responseJson = await response.json();
+  const responseJson = {
+    data: "not hired",
+  };
+  const responseStatus = 200;
+  const responseData = responseJson["data"];
+
+  if (responseStatus == 200) {
+    const mlResponse = document.getElementById("mlResponse");
+    mlResponse.innerText = responseData;
+    mlResponse.className = "response-card text-center text-capitalize";
+  } else {
+    alert(`Não foi possivel realizar a predição com o modelo. 
+    O status da resposta: ${responseStatus}.`);
+  }
+});
